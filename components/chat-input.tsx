29b042ab4e518fb8c10f289c5791f173b930c2cd
LuisKlopp <fbgus333@gmail.com>
2024-05-23 02:28:36 +0900

    Feat: socket 연결

--- components/chat-input.tsx
+++ components/chat-input.tsx
@@ -1,22 +1,29 @@
 import { Dispatch, SetStateAction } from "react";
 
 interface ChatInputProps {
-  inputValue: string;
-  setInputValue: Dispatch<SetStateAction<string>>;
+  message: string;
+  setMessage: Dispatch<SetStateAction<string>>;
+  handleSendMessage: () => void;
 }
 
-export const ChatInput = ({ inputValue, setInputValue }: ChatInputProps) => {
+export const ChatInput = ({
+  message,
+  setMessage,
+  handleSendMessage,
+}: ChatInputProps) => {
   return (
     <div className="w-full h-10 relative">
       <input
         className="input-custom w-full h-full p-2"
         placeholder="메시지 보내기"
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
         type="text"
       />
       <div className="absolute top-[10px] right-[10px]">
-        <span className="text-thickRed font-medium">보내기</span>
+        <span onClick={handleSendMessage} className="text-thickRed font-medium">
+          보내기
+        </span>
       </div>
     </div>
   );
--- components/chat.tsx
+++ components/chat.tsx
@@ -1,19 +1,56 @@
 "use client";
 
 import { ChatInput } from "@/components/chat-input";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import io, { Socket } from "socket.io-client";
+
+interface MessagePayload {
+  message: string;
+}
 
 const Chat = () => {
-  const [inputValue, setInputValue] = useState("");
+  const [socket, setSocket] = useState<Socket | null>(null);
+  const [messages, setMessages] = useState<string[]>([]);
+  const [message, setMessage] = useState<string>("");
+
+  useEffect(() => {
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+    if (!baseUrl) {
+      console.error("NEXT_PUBLIC_BASE_URL is not defined");
+      return;
+    }
+
+    const socket = io(baseUrl);
+    setSocket(socket);
+
+    socket.on("message", (payload: MessagePayload) => {
+      setMessages((prevMessages) => [...prevMessages, payload.message]);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  const handleSendMessage = () => {
+    if (socket) {
+      socket.emit("message", { message });
+      setMessage("");
+    }
+  };
 
   return (
-    <div className="w-full max-w-xl h-screen flex flex-col justify-between bg-gray-200">
+    <div className="w-full max-w-xl h-screen flex flex-col justify-between bg-[#faf0f0]">
       <div className="w-full flex justify-center p-4">
         <span className="text-lg font-bold font-jua">
-          <span className="text-gray05">ELLIO</span> 연애관 소셜링
+          <span className="text-gray05">Ellio</span> 연애 소셜링
         </span>
       </div>
-      <ChatInput inputValue={inputValue} setInputValue={setInputValue} />
+      <ChatInput
+        message={message}
+        setMessage={setMessage}
+        handleSendMessage={handleSendMessage}
+      />
     </div>
   );
 };
